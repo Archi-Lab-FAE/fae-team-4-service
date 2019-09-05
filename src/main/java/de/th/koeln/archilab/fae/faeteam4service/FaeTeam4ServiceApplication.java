f3ec46d78891c21291c15819797c52ee72578ac6
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-09-05 15:47:26 +0200

    adds spring boot project template

---
+++ src/main/java/de/th/koeln/archilab/fae/faeteam4service/FaeTeam4ServiceApplication.java
@@ -0,0 +1,13 @@
+package de.th.koeln.archilab.fae.faeteam4service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaeTeam4ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaeTeam4ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/th/koeln/archilab/fae/faeteam4service/FaeTeam4ServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.th.koeln.archilab.fae.faeteam4service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FaeTeam4ServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
