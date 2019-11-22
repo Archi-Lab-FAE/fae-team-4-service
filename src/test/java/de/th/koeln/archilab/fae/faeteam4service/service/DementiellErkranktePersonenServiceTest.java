0891d6173a4e2ab0f0978450685a701ee150f4d6
Nils Brenneis <nilsbrenneis@arcor.de>
2019-11-22 19:09:24 +0100

    organized imports

--- src/test/java/de/th/koeln/archilab/fae/faeteam4service/service/DementiellErkranktePersonenServiceTest.java
+++ src/test/java/de/th/koeln/archilab/fae/faeteam4service/service/DementiellErkranktePersonenServiceTest.java
@@ -1,22 +1,16 @@
 package de.th.koeln.archilab.fae.faeteam4service.service;
 
 import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import de.th.koeln.archilab.fae.faeteam4service.domain.DistanceInMeters;
 import de.th.koeln.archilab.fae.faeteam4service.entities.Alarmknopf;
 import de.th.koeln.archilab.fae.faeteam4service.entities.DementiellErkranktePerson;
 import de.th.koeln.archilab.fae.faeteam4service.entities.DementiellErkranktePersonRepository;
 import de.th.koeln.archilab.fae.faeteam4service.entities.position.Breitengrad;
 import de.th.koeln.archilab.fae.faeteam4service.entities.position.Laengengrad;
 import de.th.koeln.archilab.fae.faeteam4service.entities.position.Position;
-import de.th.koeln.archilab.fae.faeteam4service.service.DementiellErkranktePersonenService;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -29,15 +23,16 @@ public class DementiellErkranktePersonenServiceTest {
 
   public DementiellErkranktePersonenServiceTest() {
     mockPersonRepository = Mockito.mock(DementiellErkranktePersonRepository.class);
-    dementiellErkranktePersonenService = new DementiellErkranktePersonenService(mockPersonRepository);
+    dementiellErkranktePersonenService = new DementiellErkranktePersonenService(
+        mockPersonRepository);
   }
 
   @Before
-  public void generateTestPersonsForRepository(){
-    DementiellErkranktePerson person1 = generatePersonWithPosition("person1", 50.94239,6.97128);
-    DementiellErkranktePerson person2 = generatePersonWithPosition("person2", 50.94232,6.97138);
-    DementiellErkranktePerson person3 = generatePersonWithPosition("person3", 50.94234,6.97136);
-    DementiellErkranktePerson person4 = generatePersonWithPosition("person4", 50.94239,6.97138);
+  public void generateTestPersonsForRepository() {
+    DementiellErkranktePerson person1 = generatePersonWithPosition("person1", 50.94239, 6.97128);
+    DementiellErkranktePerson person2 = generatePersonWithPosition("person2", 50.94232, 6.97138);
+    DementiellErkranktePerson person3 = generatePersonWithPosition("person3", 50.94234, 6.97136);
+    DementiellErkranktePerson person4 = generatePersonWithPosition("person4", 50.94239, 6.97138);
 
     personen = new ArrayList<>();
     personen.add(person1);
@@ -46,49 +41,55 @@ public class DementiellErkranktePersonenServiceTest {
     personen.add(person4);
 
   }
+
   @Test
   public void givenAlarmknopfAndDementiellErkranktePersonenRepoAndRadiusThenReturnPersonenInProximity_2_InRange() {
 
-    Alarmknopf alarmknopf = generateKnopfWithIdAndPosition("id", 50.94232,6.97139);
+    Alarmknopf alarmknopf = generateKnopfWithIdAndPosition("id", 50.94232, 6.97139);
     double radius = 5.0;
 
     when(mockPersonRepository.findAll()).thenReturn(personen);
 
-    List<DementiellErkranktePerson> personenInProximity = dementiellErkranktePersonenService.getPersonenInProximityOf(alarmknopf, radius);
+    List<DementiellErkranktePerson> personenInProximity = dementiellErkranktePersonenService
+        .getPersonenInProximityOf(alarmknopf, radius);
     assertTrue(personenInProximity.contains(personen.get(1)));
     assertTrue(personenInProximity.contains(personen.get(2)));
   }
 
   @Test
   public void givenAlarmknopfAndDementiellErkranktePersonenRepoAndRadiusThenReturnPersonenInProximity_0_InRange() {
-    Alarmknopf alarmknopf = generateKnopfWithIdAndPosition("id", 78.0649,14.213);
+    Alarmknopf alarmknopf = generateKnopfWithIdAndPosition("id", 78.0649, 14.213);
     double radius = 5.0;
 
     when(mockPersonRepository.findAll()).thenReturn(personen);
 
-    List<DementiellErkranktePerson> personenInProximity = dementiellErkranktePersonenService.getPersonenInProximityOf(alarmknopf, radius);
+    List<DementiellErkranktePerson> personenInProximity = dementiellErkranktePersonenService
+        .getPersonenInProximityOf(alarmknopf, radius);
     assertTrue(personenInProximity.isEmpty());
   }
 
   @Test
   public void givenAlarmknopfAndDementiellErkranktePersonenRepoAndRadiusThenReturnPersonenInProxy_IncorrectRadius_0_InRange() {
 
-    Alarmknopf alarmknopf = generateKnopfWithIdAndPosition("id", 50.94232,6.97139);
+    Alarmknopf alarmknopf = generateKnopfWithIdAndPosition("id", 50.94232, 6.97139);
     double radius = -5.0;
 
     when(mockPersonRepository.findAll()).thenReturn(personen);
 
-    List<DementiellErkranktePerson> personenInProximity = dementiellErkranktePersonenService.getPersonenInProximityOf(alarmknopf, radius);
+    List<DementiellErkranktePerson> personenInProximity = dementiellErkranktePersonenService
+        .getPersonenInProximityOf(alarmknopf, radius);
     assertTrue(personenInProximity.isEmpty());
   }
 
-  private Alarmknopf generateKnopfWithIdAndPosition(final String id, final double latitude, final double longitude) {
+  private Alarmknopf generateKnopfWithIdAndPosition(final String id, final double latitude,
+      final double longitude) {
     Position position = getPositionFromLatitudeAndLongitude(latitude, longitude);
     return new Alarmknopf(id, position);
   }
 
-  private DementiellErkranktePerson generatePersonWithPosition(final String trackerId, final double latitude, final double longitude) {
-    Position position = getPositionFromLatitudeAndLongitude(latitude,longitude);
+  private DementiellErkranktePerson generatePersonWithPosition(final String trackerId,
+      final double latitude, final double longitude) {
+    Position position = getPositionFromLatitudeAndLongitude(latitude, longitude);
 
     return new DementiellErkranktePerson("id", trackerId, position);
   }
