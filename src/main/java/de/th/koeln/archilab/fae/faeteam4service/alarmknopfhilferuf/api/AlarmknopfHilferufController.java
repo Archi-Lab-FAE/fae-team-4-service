4269e45f13bf3ae2f89c1ee8cea11ec866cf462d
faweizz <fweiss96@googlemail.com>
2020-01-25 15:53:56 +0100

    Add custom error handling to controller

--- src/main/java/de/th/koeln/archilab/fae/faeteam4service/alarmknopfhilferuf/api/AlarmknopfHilferufController.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam4service/alarmknopfhilferuf/api/AlarmknopfHilferufController.java
@@ -2,8 +2,13 @@ package de.th.koeln.archilab.fae.faeteam4service.alarmknopfhilferuf.api;
 
 import de.th.koeln.archilab.fae.faeteam4service.alarmknopf.Alarmknopfdruck;
 import de.th.koeln.archilab.fae.faeteam4service.alarmknopf.AlarmknopfdruckService;
+import de.th.koeln.archilab.fae.faeteam4service.alarmknopf.api.AlarmknopfDto;
+import de.th.koeln.archilab.fae.faeteam4service.alarmknopfhilferuf.restpublish.MessagingServiceUnavailableException;
+import org.modelmapper.MappingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +28,9 @@ public class AlarmknopfHilferufController {
     return new ResponseEntity(HttpStatus.OK);
   }
 
+  @ExceptionHandler(MessagingServiceUnavailableException.class)
+  public ResponseEntity<String> handle(final Exception ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("Could not reach adjacent service.");
+  }
 }
