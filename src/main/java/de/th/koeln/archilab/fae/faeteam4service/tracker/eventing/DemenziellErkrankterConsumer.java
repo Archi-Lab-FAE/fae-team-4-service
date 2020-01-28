f74033b3d63bca15c126d753693145c7b5e63845
Nils Brenneis <nilsbrenneis@arcor.de>
2020-01-28 19:56:49 +0100

    kafka test
    Signed-off-by: Nils Brenneis <nilsbrenneis@arcor.de>

--- src/main/java/de/th/koeln/archilab/fae/faeteam4service/tracker/eventing/DemenziellErkrankterConsumer.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam4service/tracker/eventing/DemenziellErkrankterConsumer.java
@@ -36,8 +36,11 @@ public class DemenziellErkrankterConsumer {
       errorService.persistString("Anfang der Methode2");
       errorService.persistString("message leer? " + message.isEmpty());
       errorService.persistString("message laenge: " + message.length());
-      errorService.persistString("message1: " + message.substring(0, 250));
-      errorService.persistString("message2: " + message.substring(250));
+      errorService.persistString("message1: " + message.substring(0, 100));
+      errorService.persistString("message2: " + message.substring(100, 200));
+      errorService.persistString("message3: " + message.substring(200, 300));
+      errorService.persistString("message4: " + message.substring(300, 400));
+      errorService.persistString("message5: " + message.substring(400));
       DemenziellErkrankterEvent demenziellErkrankterEvent = objectMapper.readValue(message,
           DemenziellErkrankterEvent.class);
 
