b0fad52151404cee4cbb0564d43f34cbcbb959d5
faweizz <fweiss96@googlemail.com>
2020-01-28 14:34:38 +0100

    add timestamp of throwing

--- src/main/java/de/th/koeln/archilab/fae/faeteam4service/errorhandling/Error.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam4service/errorhandling/Error.java
@@ -3,12 +3,14 @@ package de.th.koeln.archilab.fae.faeteam4service.errorhandling;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -19,10 +21,13 @@ public class Error {
 
   @Id private String id;
 
+  private LocalDateTime thrownAt;
+
   private String text;
 
   public Error(Exception exception) {
     id = UUID.randomUUID().toString();
+    thrownAt = LocalDateTime.now();
 
     StringWriter sw = new StringWriter();
     PrintWriter pw = new PrintWriter(sw);
