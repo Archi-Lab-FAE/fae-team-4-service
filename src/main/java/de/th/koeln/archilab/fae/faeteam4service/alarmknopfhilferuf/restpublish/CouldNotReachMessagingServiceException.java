b961016dedb57c21c9ad25f2e82291fc80c2c001
faweizz <fweiss96@googlemail.com>
2020-01-30 15:28:53 +0100

    Change creation of exception

--- src/main/java/de/th/koeln/archilab/fae/faeteam4service/alarmknopfhilferuf/restpublish/CouldNotReachMessagingServiceException.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam4service/alarmknopfhilferuf/restpublish/CouldNotReachMessagingServiceException.java
@@ -1,4 +1,18 @@
 package de.th.koeln.archilab.fae.faeteam4service.alarmknopfhilferuf.restpublish;
 
+import org.springframework.web.client.RestClientException;
+
 public class CouldNotReachMessagingServiceException extends RuntimeException {
+
+  private RestClientException restClientException;
+
+  CouldNotReachMessagingServiceException(RestClientException e) {
+    this.restClientException = e;
+  }
+
+  @Override
+  public String getMessage() {
+    return "There was an Error connecting to an adjacent service. Exception: "
+        + restClientException.getMessage();
+  }
 }
