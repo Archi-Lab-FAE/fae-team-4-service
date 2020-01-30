f1a5932bb16bdf9fe4f7cf9061132936f5083153
faweizz <fweiss96@googlemail.com>
2020-01-30 19:09:47 +0100

    Revert last change

--- src/main/java/de/th/koeln/archilab/fae/faeteam4service/alarmknopfhilferuf/restpublish/MessagingServiceFeignClient.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam4service/alarmknopfhilferuf/restpublish/MessagingServiceFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(url = "https://api.fae.archi-lab.io/team-3/ausnahmesituation")
+@FeignClient(name = "fae-team-3-service")
 public interface MessagingServiceFeignClient {
 
   @RequestMapping(value = "/team-3/ausnahmesituation", method = RequestMethod.POST)
