0150e9933d9527cf36171417e72c08c120c170f8
faweizz <fweiss96@googlemail.com>
2020-01-30 13:21:31 +0100

    Add more logging

--- src/main/java/de/th/koeln/archilab/fae/faeteam4service/errorhandling/Error.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam4service/errorhandling/Error.java
@@ -25,6 +25,6 @@ public class Error {
     id = UUID.randomUUID().toString();
     thrownAt = LocalDateTime.now();
 
-    text = exception.getMessage();
+    text = exception.toString() + " " + exception.getMessage();
   }
 }
