394c74641987adfc1d1b4497d1ac47d782ea8dd1
faweizz <fweiss96@googlemail.com>
2020-01-30 14:47:23 +0100

    Add no args constructor to dto

--- src/main/java/de/th/koeln/archilab/fae/faeteam4service/alarmknopfhilferuf/AlarmknopfHilferufDto.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam4service/alarmknopfhilferuf/AlarmknopfHilferufDto.java
@@ -1,21 +1,21 @@
 package de.th.koeln.archilab.fae.faeteam4service.alarmknopfhilferuf;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import java.util.UUID;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class AlarmknopfHilferufDto {
 
   private final String id = UUID.randomUUID().toString();
 
-  private final String trackerId;
+  private String trackerId;
 
-  @JsonCreator
   public AlarmknopfHilferufDto(final String trackerId) {
     this.trackerId = trackerId;
   }
-
 }
