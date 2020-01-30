fc1afa9be51e176515f3c7252da13bf93bc0752c
faweizz <fweiss96@googlemail.com>
2020-01-30 19:07:36 +0100

    Hardcore url

--- src/main/java/de/th/koeln/archilab/fae/faeteam4service/alarmknopfhilferuf/restpublish/MessagingServiceFeignClient.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam4service/alarmknopfhilferuf/restpublish/MessagingServiceFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "fae-team-3-service")
+@FeignClient(url = "https://api.fae.archi-lab.io/team-3/ausnahmesituation")
 public interface MessagingServiceFeignClient {
 
   @RequestMapping(value = "/team-3/ausnahmesituation", method = RequestMethod.POST)
